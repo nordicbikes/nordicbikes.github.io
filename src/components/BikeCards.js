3eb9d7ccb12e52e0afa5b1e7920048352c1fce3c
codingInSpace <jonathan.grangien@gmail.com>
2016-12-12 11:41:12 +0100

    Add bike cards

---
+++ src/components/BikeCards.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Box from 'grommet/components/Box';
+import Card from 'grommet/components/Card';
+import Heading from 'grommet/components/Heading';
+
+class BikeCards extends React.Component {
+
+  render() {
+    return (
+      <Box align="center">
+        <Heading tag="h2">
+          Base Models
+        </Heading>
+        <Box
+          direction="row"
+          align="center"
+        >
+          <Box
+            basis="1/2"
+            align="end"
+            textAlign="center"
+            pad="large"
+          >
+            <Card thumbnail="/public/bike-silhouette.jpg"
+                  heading="Casual"
+                  description="Our casual base model is for you who want to be fab"
+                  headingStrong={false}
+                  textSize="large"
+                  contentPad="large" />
+          </Box>
+          <Box
+            basis="1/2"
+            align="start"
+            textAlign="center"
+            pad="large"
+          >
+            <Card thumbnail="/public/bike-silhouette.jpg"
+                  heading="Retro"
+                  description="The Retro bike is for you who want to be original"
+                  headingStrong={false}
+                  textSize="large"
+                  contentPad="large" />
+          </Box>
+        </Box>
+      </Box>
+    )
+  }
+}
+ export default BikeCards
